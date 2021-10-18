51b5d4ced31555250900bad0d8a25941fd208bde
bennett.372952 <bennett.pohlmann@gmail.com>
2021-10-18 08:43:58 -0700

    the crusade has started (texturing has been started)

--- src/com/benh3n/Main.java
+++ src/com/benh3n/Main.java
@@ -115,8 +115,6 @@ public class Main {
                 new triangle(new float[]{1, 0, 1, 0, 0, 0, 1, 0, 0})
         ));
 
-        meshCube = mesh.loadObjectFromFile("mountains.obj");
-
         running = true;
         while (running) {
             try {
